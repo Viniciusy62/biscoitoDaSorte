b56df6a3de598440ca9bd467d4c8bd25c9db23e1
Vinicius Andrade <viniciusy62@gmail.com>
2023-10-16 14:53:22 -0300

    correction in randomGenerate function

--- js/randomGenerate.js
+++ js/randomGenerate.js
@@ -2,8 +2,9 @@ import { messages } from "./messages.js";
 
 export function randomGenerate() {
   const randomNumber = Math.round(Math.random() * messages.length);
+
+  if(randomNumber == messages.length) {
+    return randomNumber - 1
+  }
   return randomNumber
 }
-
-//if(messages[i] == undefined) {}
-
